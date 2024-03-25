5b3296b779ba7cdfc557e7c912e7821b842f8583
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-03-25 16:12:56 +0530

    Added Table of Products

--- Web/frontend/react/product-hooks/src/components/productsList.tsx
+++ Web/frontend/react/product-hooks/src/components/productsList.tsx
@@ -1,29 +1,73 @@
-import products from "../data/products"
+import { useState } from "react"
+import data from "../data/products"
 import IProduct from "../model/product"
-const ProductsList = () =>{
+import Rating from "./rating"
+
+const ProductsList = () => { 
+    const [products, setProducts] = useState<IProduct[]>(data)
+    const [filteredProducts, setFilteredProducts] = useState<IProduct[]>(products)
+    const [show, setShow] = useState(false)
+
+    const title = "Products App"
+
+    const searchByName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFilteredProducts(products.filter(p => p.productName.toLocaleLowerCase().includes(e.target.value)))
+    }
+
+    const showOrHide = () => setShow(show => !show)
 
     return (
-<div id="products">
-        {
-            products.map((product:IProduct) => 
-                
-            
-         <div className="card mb-3">
-            <div className="card-body">
-            <h4 className="card-title">{product.productName}</h4>
-            <h4 className="card-title">{product.price}</h4>
-            <h3 className="card-title">{product.starRating}</h3>
-            <a href="#" className="edit card-link" data-id={product.productId}>
-            <i className="fa fa-pencil"></i>
-            </a>
-            <a href="#" className="delete card-link" data-id={product.productId}>
-            <i className="fa fa-remove"></i>
-            </a>
+        <div className='card'>
+            <div className='card-header'>
+                {title}
             </div>
+            <div className='card-body'>
+                <div className='row'>
+                    <div className='col-md-2'>Filter by:</div>
+                    <div className='col-md-4'>
+                        <input type='text' onChange={searchByName} />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <h4>Filtered by: </h4>
+                    </div>
+                </div>
+                <div className='table-responsive'>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>
+                                    <button className='btn btn-primary' onClick={showOrHide}>
+                                        {show ? "Hide" : "Show"} Image
+                                    </button>
+                                </th>
+                                <th>Product</th>
+                                <th>Code</th>
+                                <th>Available</th>
+                                <th>Price</th>
+                                <th>5 Star Rating</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredProducts.map(product =>
+                                <tr>
+                                    <td>
+                                        {show && <img src={product.imageUrl} title={product.productName} className="avatar" style={{ width: 75, margin: 2 }} />}
+                                    </td>
+                                    <td><a>{product.productName}</a></td>
+                                    <td>{product.productCode}</td>
+                                    <td>{product.productAvailable}</td>
+                                    <td>{product.price}</td>
+                                    <td><Rating rating={product.starRating} /></td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            )
-        }
         </div>
+
     )
 
 }
