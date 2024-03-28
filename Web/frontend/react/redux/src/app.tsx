ff3f9265b8be04f0db6a0083ed0a06f8eb4112f7
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-03-28 15:26:18 +0530

    Sidebar Navigation

--- Web/frontend/react/redux/src/app.tsx
+++ Web/frontend/react/redux/src/app.tsx
@@ -1,29 +1,19 @@
-import { Home, Sidebar, Headphones, Search } from "react-feather";
+import { Home, Sidebar, Headphones } from "react-feather";
 import { AnimatePresence, motion, } from "framer-motion"
 import useAppStore from "./store";
+import NavItem from "./components/NavItem";
 
-export default function App() {
-  const { isSidebarOpen, toggleSidebar } = useAppStore()
-  return <main className="flex items-start">
+export default function App({ children }: { children: React.ReactNode }) {
+  return <>
     <NavigationBar />
-    <header className="flex-grow">
-      {!isSidebarOpen && (
-        <button onClick={toggleSidebar} >
-          <Sidebar size={20} color="#0066FF" />
-        </button>
-      )}
-      <div className="flex-grow"></div>
-      <button onClick={toggleSidebar} >
-        <Search size={20} color="#0066FF" />
-      </button>
-    </header>
-  </main>
+    {children}
+  </>
 
 
 }
 
 function NavigationBar() {
-  const width = 300;
+  const width = 200;
   const { isSidebarOpen, toggleSidebar } = useAppStore()
 
   return <AnimatePresence initial={false}>
@@ -36,13 +26,14 @@ function NavigationBar() {
         animate={{ width: [0, width] }} exit={{ width: [width, 0] }}
         className="bg-gray-100 border-r h-screen flex flex-row-reverse overflow-hidden">
         <div
-          className="flex flex-col gap-4"
+          className="flex flex-col gap-3"
           style={{
             minWidth: width,
             maxWidth: width,
             width: width
           }}>
           <header>
+            <div className="flex-grow" />
             {!isSidebarOpen && (
               <button onClick={toggleSidebar} >
                 <Sidebar size={16} color="#0066FF" />
@@ -52,10 +43,9 @@ function NavigationBar() {
               <Sidebar size={20} color="#0066FF" />
             </button>
           </header>
-          <h2 className="text-3xl font-bold px-4">Products</h2>
-          <ul className="px-4">
-            <li className="flex p-3 items-center gap-3 text-white bg-accent rounded-xl"><Home color="#FFFFFF" /> Home</li>
-            <li className="flex p-3 items-center gap-3"><Headphones color="#0066FF" /> Home</li>
+          <ul className="px-3">
+            <NavItem to="/" Icon={Home} label="Home" />
+            <NavItem to="/products" Icon={Headphones} label="Products" />
           </ul>
         </div>
 
--- Web/frontend/react/redux/src/store.tsx
+++ Web/frontend/react/redux/src/store.tsx
@@ -1,4 +1,4 @@
-import { State, create } from "zustand";
+import { create } from "zustand";
 
 type StateObject = {
   isSidebarOpen: boolean
