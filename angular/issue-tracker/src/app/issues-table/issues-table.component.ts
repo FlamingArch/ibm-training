6724f0f15575aa1bcc3e0cf34092bcbe108293d9
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-04-05 16:58:06 +0530

    Issue Tracker Init, Issues Table

---
+++ angular/issue-tracker/src/app/issues-table/issues-table.component.ts
@@ -0,0 +1,34 @@
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core';
+import { Issue, IssueType, IssuePriority } from '../types';
+
+let issueData: Issue[] = [
+  {
+    issueNo: 1,
+    type: IssueType.Bug,
+    title: 'Sample Issue',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor iusto hic quos quia, vero ullam ipsam enim quam ducimus debitis.',
+    priority: IssuePriority.High,
+  },
+];
+
+@Component({
+  selector: 'app-issues-table',
+  standalone: true,
+  imports: [MatTableModule, MatIconModule, MatButtonModule],
+  templateUrl: './issues-table.component.html',
+  styleUrl: './issues-table.component.scss',
+})
+export class IssuesTableComponent {
+  displayedColumns: string[] = [
+    'issueNo',
+    'type',
+    'title',
+    'description',
+    'priority',
+  ];
+  issues = issueData;
+}
