6639043457c03bc77bc1b16687e5ca1a68606a24
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-04-08 16:01:23 +0530

    Express Server POST

--- express/app.js
+++ express/app.js
@@ -3,6 +3,7 @@ import productRoutes from "./routes/product.js";
 
 const app = express();
 
+app.use(express.json());
 app.use("/ibm", productRoutes);
 
 app.listen(9999, () => {
--- express/routes/product.js
+++ express/routes/product.js
@@ -7,5 +7,14 @@ router.get("/api/products", (req, res) => {
   res.json(products);
 });
 
+router.post("/api/products", (req, res) => {
+  const newProduct = req.body;
+
+  if (!newProduct.productName || !newProduct.price) {
+    res.status(400).json({ message: "Product Name and Price is required" });
+  }
+  products.push(newProduct);
+  res.json(products);
+});
 
 export default router;
