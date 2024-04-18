b643a188bb400603c4d2bca3aae8e3d9304b89d6
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-04-18 12:20:28 +0530

    Exceptions and Exception IO

--- java/CoreJava/src/com/myapp/core/MarkerImpl.java
+++ java/CoreJava/src/com/myapp/core/MarkerImpl.java
@@ -2,15 +2,21 @@ package com.myapp.core;
 
 public class MarkerImpl implements Marker{
 	
-	public void checkInterface() {
+	public void checkInterface() throws InterfaceNotFoundException {
 		if (Marker.class.isInstance(this)) { 
 			System.out.println("It implements Marker");
-		} else System.out.println("It doesn't implement Marker");
+		} else throw new InterfaceNotFoundException("It doesn't implement Marker");
 	}
 
 	public static void main(String[] args) {
 		MarkerImpl marker = new MarkerImpl();
-		marker.checkInterface();
+		try {
+			marker.checkInterface();
+		} catch (InterfaceNotFoundException e){
+			e.printStackTrace();
+		} finally {
+			System.out.println("Inside Finally");
+		}
 	}
 
 }
